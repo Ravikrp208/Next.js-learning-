7ca127506a0a37076d41c6659a95cfd600d94a40
Ravikrp208 <ravikrp208@gmail.com>
2026-06-09 21:15:10 +0530

    MCP server create in tiny cats

--- Tinay-cat/Backend/src/controller/test-mcp.controller.ts
+++ Tinay-cat/Backend/src/controller/test-mcp.controller.ts
@@ -1,38 +1,40 @@
 import type { Request, Response } from "express";
 import { getMcpClient } from "../services/mcp.service.ts";
-import { recommendCatsController } from "./cat.controller.ts";
 import { generateAiResponse } from "../services/gemini.service.ts";
 
 export const testMcpController = async (req: Request, res: Response) => {
-
+  try {
     const client = await getMcpClient();
 
-    const tools = await client.listTools();
-
     const result = await client.callTool({
-        name: "recommend_cats",
-        arguments: {
-            kidsFriendly: true,
-            apartmentFriendly: false
-        }
-    })
-    let catsData = result.content[0].text;
-    let prompt = `
-      
-      Avaible cats 
-      ${catsData}
+      name: "recommend_cats",
+      arguments: {
+        kidsFriendly: true,
+        apartmentFriendly: false,
+      },
+    });
 
-      recommend best cats from this data
-        `;
+    const catsData = result.content[0].text;
 
+    const prompt = `
+Available cats:
+${catsData}
 
-    let aiResponse = await generateAiResponse(prompt);
+Recommend the best cats from this data.
+`;
 
+    const aiResponse = await generateAiResponse(prompt);
 
     return res.json({
-        success: true,
-        // result
-        data: catsData,
-    })
-
-}
+      success: true,
+      catsData,
+      aiResponse,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "MCP or AI error",
+      error,
+    });
+  }
+};
