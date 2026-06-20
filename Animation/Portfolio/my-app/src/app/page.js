860a60f3970254a270ea14e210bc92ccd004b157
Ravikrp208 <ravikrp208@gmail.com>
2026-06-20 23:53:00 +0530

    feat: add Portfolio Next.js app and update note.txt

---
+++ Animation/Portfolio/my-app/src/app/page.js
@@ -0,0 +1,17 @@
+"use client";
+
+import InfiniteCarousel from "@/components/InfiniteCarousel";
+import TextReveal from "@/components/TextReveal";
+import { projects } from "@/data/projects";
+import { useRef } from "react";
+
+
+export default function Home() {
+  
+
+  return (
+    <main className="h-screen flex items-start w-full">
+      <InfiniteCarousel projects={projects} />
+    </main>
+  );
+}
